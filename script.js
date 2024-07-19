a4d41d277bbe9451ab8b4f7777c3666b18f14459
Marta GD <marta.483@hotmail.com>
2024-07-19 19:56:43 +0200

    Maquetacion v0.5

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ function myFunction(){
     let aux = fileS.length;
     document.getElementById("pueba").innerHTML = aux;
 }
+
+function checkButtonToTop(){
+    if(window.scrollY > 0){
+        document.getElementById('toTheTop').hidden = false;
+    }else{
+        document.getElementById('toTheTop').hidden = true;
+    }
+}
+
+var el = document.getElementById('toTheTop');
+el.addEventListener('scroll', function(event) {
+    if (event.target.scrollTop === 0) {
+        // Top of the page detected
+        // You can perform your desired action here
+    }
+});
